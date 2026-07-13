5d5c70e93dea98f7395381fd5f8e349d333ae94f
Yash Tripathi <yashtripathi2400@gmail.com>
2026-07-14 00:18:20 +0530

    Add maxDistinct function for distinct substrings
    Implement a function to count distinct starting substrings.

--- LeetCode/3760. Maximum Number of Distinct Starting Substrings/solution.cpp
+++ LeetCode/3760. Maximum Number of Distinct Starting Substrings/solution.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        string s1;
+        s1.push_back(s[0]);
+        for(int i=1;i<s.size();i++){
+        bool found = false;
+            for(int j=0;j<s1.size();j++){
+               if(s[i] ==s1[j]){
+                found = true;
+                break;
+               }
+               
+            }
+            if(found == false){
 
+                s1.push_back(s[i]);
+            }
+        }
+        return s1.size();
+    }
+
+};
