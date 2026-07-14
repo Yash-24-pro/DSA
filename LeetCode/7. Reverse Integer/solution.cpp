68aae3db942bbedbb589ab92c300db4788bd226a
Yash Tripathi <yashtripathi2400@gmail.com>
2026-07-14 18:22:57 +0530

    Implement reverse integer function

---
+++ LeetCode/7. Reverse Integer/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int reverse = 0;
+        while (x != 0) {
+            int rem;
+            rem = x % 10;
+            x = x / 10;
+            if (reverse > INT_MAX / 10 || (reverse == INT_MAX / 10 && rem > 7))
+                return 0;
+
+            if (reverse < INT_MIN / 10 || (reverse == INT_MIN / 10 && rem < -8))
+                return 0;
+            reverse = reverse * 10 + rem;
+        }
+        return reverse;
+    }
+};
